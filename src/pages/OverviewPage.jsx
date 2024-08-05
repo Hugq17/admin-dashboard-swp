568bd86eef82a44ecc1be5908bc7d0c496104875
hung1720 <coderlazy17@gmail.com>
2024-08-06 01:21:23 +0700

    style sidebar
    25.55

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OverviewPage = () => {
+  return <div className="text-pink-500">OverviewPage</div>;
+};
+
+export default OverviewPage;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsPage = () => {
+  return <div className="text-pink-500">ProductsPage</div>;
+};
+
+export default ProductsPage;
